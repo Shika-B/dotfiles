7dde9c791647149cdf1c3fabde6c221a38ede236
Shika Blyat <abdelzighem10@gmail.com>
2022-08-04 06:45:11 +0200

    COmplete installs.sh

--- installs.sh
+++ installs.sh
@@ -1,5 +1,26 @@
+sudo pacman -Syu
+
 sudo pacman -S --needed polybar alacritty rofi
+
 yay -S --needed pulseaudio-control
+
+# Comment the lines before the "Manage dotfiles" 
+# if you don't want my personal packages that have nothing
+# to do with my linux setup
+
+sudo pacman -S --needed firefox code discord sagemath
+sudo pacman -S texlive-core texlive-science texlive-pictures
+
+yay -S --needed rustup && rustup default stable
+
+yay -S -needed sublime-text-4 obsidian
+
+
+echo "Fixing discord's .desktop file"
+cat discord.desktop > /usr/share/applications/discord.desktop
+
+
+# Manage dotfiles
 
 declare -a configs=("alacritty" "rofi" "polybar")
 
@@ -13,3 +34,5 @@ do
 	fi
 	ln -s ~/dotfiles/$conf ~/.config/$conf
 done
+
+
